261b2f8a29509afb8e4a12525873c896603d2646
Collin Tobin <ctobin@adobe.com>
2023-12-13 15:33:25 -0500

    works with aus5

--- code/app/src/main/java/com/adobe/cuc/e2e/messagingdemoapp/MessagingApplication.kt
+++ code/app/src/main/java/com/adobe/cuc/e2e/messagingdemoapp/MessagingApplication.kt
@@ -22,7 +22,7 @@ import com.google.firebase.messaging.FirebaseMessaging
 
 
 class MessagingApplication : Application() {
-    private val ENVIRONMENT_FILE_ID = "bf7248f92b53/caf1bf1baeb1/launch-2ccf360c056d-development"
+    private val ENVIRONMENT_FILE_ID = "3269cfd2f1f9/bf87ecce10d2/launch-f2391ecada3e-development"
     private val ASSURANCE_SESSION_ID = "messagingdemo://foo?adb_validation_sessionid=71ae2ff9-9d35-4877-973a-0cab33e716d8"
     override fun onCreate() {
         super.onCreate()
